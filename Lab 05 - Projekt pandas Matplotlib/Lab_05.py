0f0adf39c91983b588ab2674eb27cb4f8bb8e1c2
pawel-gawron <gawron.pawel1999@gmail.com>
2023-11-21 21:16:08 +0100

    add notebook for testing and data loader

--- Lab 05 - Projekt pandas Matplotlib/Lab_05.py
+++ Lab 05 - Projekt pandas Matplotlib/Lab_05.py
@@ -1,4 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import glob
+import os
 
+folder_path = './data/names'
+
+file_list = glob.glob(os.path.join(folder_path, '*.txt'))
+
+names_df = pd.DataFrame()
+names_df_list = []
+
+for file in file_list:
+    data = pd.read_csv(file)
+    names_df_list.append(data)
+
+names_df = pd.concat(names_df_list, ignore_index=True)
+
+print(names_df)
